77184eab23b10853ba62a4ee3656930193f8290e
Daniel Fritsch <daniel.fritsch@raysono.com>
2020-02-14 09:47:38 +0100

    Refactoring vom SettingsManager

--- app/src/main/java/com/daniel/mobilepauker2/settings/SettingsManager.kt
+++ app/src/main/java/com/daniel/mobilepauker2/settings/SettingsManager.kt
@@ -14,8 +14,8 @@ import com.daniel.mobilepauker2.R
  */
 class SettingsManager {
 
-    fun getSettingsKey(context: Context, key: Keys): String {
-        return when (key) {
+    fun getSettingsKey(context: Context, key: Keys): String =
+        when (key) {
             Keys.STM -> context.getString(R.string.stm_key)
             Keys.USTM -> context.getString(R.string.ustm_key)
             Keys.ABOUT -> context.getString(R.string.about)
@@ -35,10 +35,9 @@ class SettingsManager {
             Keys.RING_TONE -> context.getString(R.string.ring_tone_preference)
             Keys.SHOW_TIMER_BAR -> context.getString(R.string.show_timer_bar)
         }
-    }
 
-    private fun getDefaultStringValue(context: Context, key: Keys): String {
-        return when (key) {
+    private fun getDefaultStringValue(context: Context, key: Keys): String =
+        when (key) {
             Keys.STM -> context.getString(R.string.stm_default)
             Keys.USTM -> context.getString(R.string.ustm_default)
             Keys.REPEAT_CARDS -> context.getString(R.string.repeat_cards_default)
@@ -46,30 +45,22 @@ class SettingsManager {
             Keys.RETURN_FORGOTTEN_CARDS -> context.getString(R.string.return_forgotten_cards_default)
             else -> ""
         }
-    }
 
-    private fun getDefaultBoolValue(context: Context, key: Keys): Boolean {
-        return when (key) {
-            Keys.AUTO_DOWNLOAD -> context.resources
-                .getBoolean(R.bool.auto_download_default)
+    private fun getDefaultBoolValue(context: Context, key: Keys): Boolean =
+        when (key) {
+            Keys.AUTO_DOWNLOAD -> context.resources.getBoolean(R.bool.auto_download_default)
             Keys.AUTO_SAVE -> context.resources.getBoolean(R.bool.auto_save_default)
             Keys.AUTO_UPLOAD -> context.resources.getBoolean(R.bool.auto_upload_default)
             Keys.HIDE_TIMES -> context.resources.getBoolean(R.bool.auto_upload_default)
-            Keys.CASE_SENSITIV -> context.resources
-                .getBoolean(R.bool.auto_upload_default)
-            Keys.LEARN_NEW_CARDS_RANDOMLY -> context.resources
-                .getBoolean(R.bool.learn_new_cards_randomly_default)
-            Keys.ENABLE_EXPIRE_TOAST -> context.resources
-                .getBoolean(R.bool.expire_toast_default)
-            Keys.SHOW_TIMER_NOTIFY -> context.resources
-                .getBoolean(R.bool.show_timer_notification_default)
-            Keys.SHOW_CARD_NOTIFY -> context.resources
-                .getBoolean(R.bool.show_card_notification_default)
-            Keys.SHOW_TIMER_BAR -> context.resources
-                .getBoolean(R.bool.show_timer_bar_default)
+            Keys.CASE_SENSITIV -> context.resources.getBoolean(R.bool.auto_upload_default)
+            Keys.LEARN_NEW_CARDS_RANDOMLY -> context.resources.getBoolean(R.bool.learn_new_cards_randomly_default)
+            Keys.ENABLE_EXPIRE_TOAST -> context.resources.getBoolean(R.bool.expire_toast_default)
+            Keys.SHOW_TIMER_NOTIFY -> context.resources.getBoolean(R.bool.show_timer_notification_default)
+            Keys.SHOW_CARD_NOTIFY -> context.resources.getBoolean(R.bool.show_card_notification_default)
+            Keys.SHOW_TIMER_BAR -> context.resources.getBoolean(R.bool.show_timer_bar_default)
             else -> false
         }
-    }
+
 
     fun getBoolPreference(context: Context, key: Keys): Boolean {
         val prefKey = getSettingsKey(context, key)
@@ -80,8 +71,8 @@ class SettingsManager {
     fun getStringPreference(context: Context, key: Keys): String {
         val prefKey = getSettingsKey(context, key)
         val defValue = getDefaultStringValue(context, key)
-        return PreferenceManager.getDefaultSharedPreferences(context).getString(prefKey, defValue)
-            ?: ""
+        return PreferenceManager
+            .getDefaultSharedPreferences(context).getString(prefKey, defValue) ?: ""
     }
 
     enum class Keys {
