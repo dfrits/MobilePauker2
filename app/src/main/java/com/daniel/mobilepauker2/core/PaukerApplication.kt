df41a52abfeec65c5bb5e53a63da188a0dfc386e
danielfritsch <fritsch_daniel@gmx.de>
2020-08-11 16:22:15 +0200

    fix crash

--- app/src/main/java/com/daniel/mobilepauker2/core/PaukerApplication.kt
+++ app/src/main/java/com/daniel/mobilepauker2/core/PaukerApplication.kt
@@ -1,8 +1,10 @@
 package com.daniel.mobilepauker2.core
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.daniel.mobilepauker2.di.appModule
+import com.daniel.mobilepauker2.main.MainMenu
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
@@ -16,5 +18,8 @@ open class PaukerApplication : AppCompatActivity() {
             androidContext(baseContext)
             modules(appModule)
         }
+
+        startActivity(Intent(this, MainMenu::class.java))
+        finish()
     }
 }
