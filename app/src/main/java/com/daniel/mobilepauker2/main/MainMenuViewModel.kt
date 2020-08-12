24dae0e565d17b1c00aa92327f1a821b8535a7d5
danielfritsch <fritsch_daniel@gmx.de>
2020-08-12 11:11:00 +0200

    Add ViewModel

--- app/src/main/java/com/daniel/mobilepauker2/main/MainMenuViewModel.kt
+++ app/src/main/java/com/daniel/mobilepauker2/main/MainMenuViewModel.kt
@@ -1,8 +1,19 @@
 package com.daniel.mobilepauker2.main
 
+import android.content.Context
 import androidx.lifecycle.ViewModel
+import com.daniel.mobilepauker2.core.PaukerManager
+import com.daniel.mobilepauker2.pauker_native.ErrorReporter
+import com.daniel.mobilepauker2.pauker_native.ModelManager
+import com.daniel.mobilepauker2.settings.SettingsManager
 
-class MainMenuViewModel : ViewModel() {
+class MainMenuViewModel(
+        val modelManager: ModelManager,
+        val paukerManager: PaukerManager,
+        val settingsManager: SettingsManager,
+        val errorReporter: ErrorReporter,
+        val context: Context
+) : ViewModel() {
 
 
 }
